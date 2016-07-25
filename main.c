6bacbd91d735d11beccad137be1fb09cf31f6863
adigrabow <adigrabow@Adis-MacBook-Air.local>
2016-07-25 09:25:45 +0300

    main file with tests

---
+++ main.c
@@ -0,0 +1,113 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 23, 2016
+ *      Author: adigrabow
+ */
+#include "SPPoint.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include "KDArray.h"
+
+
+void testForMemCpy();
+void printMatrix(int** mat, int numOfPoints, int dim);
+void printKDArray(kdArray arr, int size);
+
+int main(){
+
+
+	//testForMemCpy();
+	printf("entered main\n");
+	double data1[2] = {1.0,2.0};
+	double data2[2]= {123.0,70.0};
+	double data3[2]= {2.0,7.0};
+	double data4[2]= {9.0,11.0};
+	double data5[2]= {3.0,4.0};
+
+
+	SPPoint p1 = spPointCreate(data1, 2, 1);
+	SPPoint p2 = spPointCreate(data2, 2, 2);
+	SPPoint p3 = spPointCreate(data3, 2, 3);
+	SPPoint p4 = spPointCreate(data4, 2, 4);
+	SPPoint p5 = spPointCreate(data5, 2, 5);
+
+
+	SPPoint SPPointArr[5] = {p1,p2,p3,p4,p5};
+	printf("before Init\n");
+	kdArray resultArray = Init(SPPointArr , 5);
+	printf("after Init\n");
+
+	printMatrix(getMatrixFromKDArray(resultArray), 5, 2);
+
+	printf("finished main!\n");
+	return 0;
+}
+
+void testForMemCpy(){
+
+	double data1[4] = {1.1,2.2,3.3,4.4};
+	double data2[4]= {5.5,6.6,7.7,8.8};
+	double data3[4]= {9.9,10.10,11.11,12.12};
+
+	SPPoint p1 = spPointCreate(data1, 4, 1);
+	SPPoint p2 = spPointCreate(data2, 4, 2);
+	SPPoint p3 = spPointCreate(data3, 4, 3);
+
+	SPPoint SPPointArr[3] = {p1,p1,p3};
+
+	for(int i = 0; i < 3; i++){
+		printf("p%d[0] = %f\n",i,spPointGetAxisCoor(SPPointArr[i], 0));
+	}
+
+	SPPoint* copiedArr = (SPPoint*)malloc(sizeof(SPPointArr));
+	memcpy(copiedArr,SPPointArr, sizeof(SPPointArr));
+
+	for(int i = 0; i < 3; i++){
+		printf("p%d[0] = %f\n",i,spPointGetAxisCoor(copiedArr[i], 0));
+	}
+	return;
+}
+
+
+void printMatrix(int** mat, int numOfPoints, int dim){
+	printf("entered printMatrix\n");
+	for(int i = 0; i < dim ; i++){
+		printf("row %d: ",i);
+		for(int j = 0 ; j < numOfPoints; j++){
+			printf("%d,",mat[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void testSplit1(){
+	double data1[2] = {1.0,2.0};
+	double data2[2]= {123.0,70.0};
+	double data3[2]= {2.0,7.0};
+	double data4[2]= {9.0,11.0};
+	double data5[2]= {3.0,4.0};
+
+
+	SPPoint p1 = spPointCreate(data1, 2, 1);
+	SPPoint p2 = spPointCreate(data2, 2, 2);
+	SPPoint p3 = spPointCreate(data3, 2, 3);
+	SPPoint p4 = spPointCreate(data4, 2, 4);
+	SPPoint p5 = spPointCreate(data5, 2, 5);
+
+	SPPoint SPPointArr[5] = {p1,p2,p3,p4,p5};
+	kdArray resultArray = Init(SPPointArr , 5);
+//	kdArray leftKD = split(...)
+	//sprintKDArray(leftKD, (resultArray->size)/2);
+
+	return;
+}
+
+void printKDArray(kdArray arr, int size){
+	printMatrix(getMatrixFromKDArray(arr), size, getDimFromKDArray(arr));
+	return;
+}
+
