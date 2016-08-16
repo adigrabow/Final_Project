f13d0e34ed24973f1c7e8749dc54856fedcbf955
sivroni <sivroni@mail.tau.ac.il>
2016-08-16 12:21:37 +0300

    added SPsplitMethod to init function

--- KDTree.h
+++ KDTree.h
@@ -4,11 +4,20 @@
  *  Created on: 27 αιεμ 2016
  *      Author: Maayan Sivroni
  */
-
+#include "SPConfig.h"
 
 
 typedef struct KDTreeNode* kdTree;
-kdTree init(kdArray kdArr, int * index);
+kdTree init(kdArray kdArr, int * index, SP_KDTREE_SPLIT_METHOD_TYPE splitMethod);
 
 
+int kdTreeGetDimension(kdTree node);
+double kdTreeGetVal(kdTree node);
+kdTree  kdTreeGetLeft(kdTree node);
 
+kdTree  kdTreeGetRight(kdTree node);
+
+//SPPoint * kdTreeGetData(kdTree node);
+SPPoint kdTreeGetData(kdTree node);
+void destroyKdTree(kdTree node);
+void printPointIndex(kdArray arr, int numOfPoints);
